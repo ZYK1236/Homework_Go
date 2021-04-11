ed82f7590a366235bd99c42f7c53865aae75da0d
ZYK <15986875679@163.com>
2021-04-11 17:57:08 +0800

    🐛feat: 上线前的准备

--- src/database/config.go
+++ src/database/config.go
@@ -12,7 +12,7 @@ var DB *sqlx.DB
 func Init() {
 	var err error
 	//定义mysql数据源，配置数据库地址，帐号以及密码
-	dsn := "homeworkgo:DF354NH2!69iuis@tcp(homeworkgo.mysql.polardb.rds.aliyuncs.com)/homeworkgo?charset=utf8&parseTime=True&loc=Local"
+	dsn := "账号:密码(homeworkgo.mysql.polardb.rds.aliyuncs.com)/homeworkgo?charset=utf8&parseTime=True&loc=Local"
 
 	//根据数据源dsn和mysql驱动, 创建数据库对象
 	DB, err = sqlx.Open("mysql", dsn)
@@ -21,4 +21,3 @@ func Init() {
 	}
 	fmt.Println("数据库启动成功 ✅: --->", DB)
 }
-
