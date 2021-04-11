1e68936f6acbc4e3cbc3bede0c8e14ba1c895570
ZYK <15986875679@163.com>
2021-04-11 17:27:16 +0800

    👴 feat: 完成所有接口调试（乞丐版）

--- src/controller/findteacher.go
+++ src/controller/findteacher.go
@@ -1,3 +1,11 @@
+/**
+  ******************************************************************************
+  * File Name          : 查询所有老师 Controller
+  * Author             : 张宇恺
+  * Description        : 查表，给出表中所有老师 id 以及名字
+  ******************************************************************************
+*/
+
 package controller
 
 import (
@@ -20,7 +28,7 @@ func (fc *FindTeacherController) GetTeacher(ctx iris.Context) model.ResponseMode
 	sql := "select id, teachername from teacher;"
 	err := database.DB.Select(&result, sql)
 	if err != nil {
-		fmt.Print("database.DB.Select error 查找所有课程 id 失败 ❌", err.Error())
+		fmt.Print("database.DB.Select error 查找所有老师 id 失败 ❌", err.Error())
 		return model.ResponseModel{
 			Data: nil,
 			Code: 0,
