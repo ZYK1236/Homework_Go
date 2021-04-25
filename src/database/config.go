b8528464d606ce37c22e59657a3fdf0b95864afa
ZYK <15986875679@163.com>
2021-04-25 22:49:31 +0800

    🐛fix: change database config

--- src/database/config.go
+++ src/database/config.go
@@ -12,7 +12,7 @@ var DB *sqlx.DB
 func Init() {
 	var err error
 	//定义mysql数据源，配置数据库地址，帐号以及密码
-	dsn := "homeworkgo:DF354NH2!69iuis@tcp(homeworkgo.mysql.polardb.rds.aliyuncs.com)/homeworkgo?charset=utf8&parseTime=True&loc=Local"
+	dsn := "@tcp(homeworkgo.mysql.polardb.rds.aliyuncs.com)/homeworkgo?charset=utf8&parseTime=True&loc=Local"
 
 	//根据数据源dsn和mysql驱动, 创建数据库对象
 	DB, err = sqlx.Open("mysql", dsn)
